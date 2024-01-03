feaea26a7b10b0bd67b26713cebf134727a99eda
Galeev Timur <timurdev@whitelevel.eu>
2024-01-03 12:39:31 +0300

    add logger

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,40 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
+
+	"github.com/gtgaleevtimur/alice-skill/internal/logger"
 )
 
-func main() {
-	// обрабатываем аргументы командной строки
-	parseFlags()
-
-	if err := run(); err != nil {
-		panic(err)
-	}
-}
-
 func run() error {
-	fmt.Println("Running server on", flagRunAddr)
-	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
+	if err := logger.Initialize(flagLogLevel); err != nil {
+		return err
+	}
+
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+	// оборачиваем хендлер webhook в middleware с логированием
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		// разрешаем только POST-запросы
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// установим правильный заголовок для типа данных
 	w.Header().Set("Content-Type", "application/json")
-	// пока установим ответ-заглушку, без проверки ошибок
 	_, _ = w.Write([]byte(`
-      {
-        "response": {
-          "text": "Извините, я пока ничего не умею"
-        },
-        "version": "1.0"
-      }
-    `))
+          {
+            "response": {
+              "text": "Извините, я пока ничего не умею"
+            },
+            "version": "1.0"
+          }
+        `))
+	logger.Log.Debug("sending HTTP 200 response")
 }
